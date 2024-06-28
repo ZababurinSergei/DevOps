3450cc32f32c0ad3526ee90c24b9015e36b4839c
Zababurin <zababurinsv@vk.com>
2024-06-28 13:46:42 +0300

    11111-1

--- sw.mjs
+++ sw.mjs
@@ -219,10 +219,29 @@ self.addEventListener('fetch', event => {
                 }
             }) ());
         } else {
-            if(!isHtml) {
-                return url.href
-            }
-            console.log('@@@@@@@@@@@@@@@@@@@@@@ 3 @@@@@@@@@@@@@@@@@@@@@@', url)
+            event.respondWith(
+                fetch(event.request)
+                    .then(function (response) {
+                        // It seems like we only need to set the headers for index.html
+                        // If you want to be on the safe side, comment this out
+                        // if (!response.url.includes("index.html")) return response;
+
+                        const newHeaders = new Headers(response.headers);
+                        newHeaders.set("Cross-Origin-Embedder-Policy", "require-corp");
+                        newHeaders.set("Cross-Origin-Opener-Policy", "same-origin");
+
+                        const moddedResponse = new Response(response.body, {
+                            status: response.status,
+                            statusText: response.statusText,
+                            headers: newHeaders,
+                        });
+
+                        return moddedResponse;
+                    })
+                    .catch(function (e) {
+                        console.error(e);
+                    })
+            );
         }
     } else {
         console.log('@@@@@@@@@@@@@@@@@@@@@@ 66 @@@@@@@@@@@@@@@@@@@@@@', event.request)
