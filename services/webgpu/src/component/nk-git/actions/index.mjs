b3c800a2bef9816d76088be06c29c721e3255925
Zababurin <zababurinsv@vk.com>
2024-06-28 13:11:29 +0300

    11111

--- services/webgpu/src/component/nk-git/actions/index.mjs
+++ services/webgpu/src/component/nk-git/actions/index.mjs
@@ -37,8 +37,6 @@ export const actions = (self) => {
                     }
 
                     self.task = {
-                        id: self.tagName.toLowerCase(),
-                        uuid: self.dataset.uuid,
                         component: 'nk-opfs',
                         type: 'self',
                         action: 'default',
@@ -62,17 +60,17 @@ export const actions = (self) => {
                             const iframe = document.createElement('iframe');
 
                             iframe.setAttribute('seamless', '');
-                            iframe.src = `${window.location.origin}/DevOps/sw/index.git.html`;
+                            iframe.src = `${window.location.origin}/sw/index.git.html`;
                             self.html.views.run.appendChild(iframe);
                             self.html.control.button.run.classList.add('disabled');
                             self.html.control.button.clear.classList.remove('disabled');
 
-                            history.pushState({}, '', '/DevOps/sw/');
+                            history.pushState({}, '', '/sw/');
 
                             iframe.addEventListener('load', function(e) {
-                                iframe.contentWindow.postMessage({
-                                    html: html
-                                });
+                                // iframe.contentWindow.postMessage({
+                                //     html: html
+                                // });
                             });
                         }
                     };
